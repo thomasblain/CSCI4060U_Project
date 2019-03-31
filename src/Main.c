76493a8b2eb9530fcdd2c0768dc0ef59e4df3be1
thomasblain <thomas.blain@uoit.net>
2019-03-31 15:41:06 -0400

    program will now output to new zipped file

--- src/Main.c
+++ src/Main.c
@@ -13,9 +13,10 @@ const char output_file_type[] = ".bzip";
 char buffer_output[buffer_size];
 int output = 0;
 
-int zipRLE(char* filename);
 int zip(char* filename);
 int unzipRLE(char* filename);
+int save_buffer(char* filename);
+char* swap_extension(char* file);
 
 int main(int argc, char** argv) {
   if (argc != 3) {
@@ -57,7 +58,7 @@ void output_run(int count, char run) {
     for (int i = 0; i < num_digits; i++) {
       buffer_output[output++] = num[i];
     }
-  } else buffer_output[output++] = count;
+  } else { buffer_output[output++] = count + '0'; }
 
   buffer_output[output++] = run;
   printf("Buffer; %s\n", buffer_output);
@@ -81,6 +82,7 @@ int zip(char * filename) {
         buffer_output[output++] = '\n';
         printf("Final buffer: %s\n", buffer_output);
         output = 0;
+        save_buffer(filename);
         break;
       }
 
@@ -101,98 +103,40 @@ int zip(char * filename) {
       }
     }
   }
+  fclose(file_input);
 
   return 0;
 }
 
-int zipRLE(char* filename) {
-  printf("Zipping %s...\n", filename);
+int save_buffer(char* filename) {
+  FILE *file_output;
 
-  // char* dir_string = strdup(filename);
-  // char* file_string = strdup(filename);
-  //
-  // char* dir = dirname(dir_string);
-  // char* file_input_name = basename(file_string);
-  //
-  // printf("%s\t%s\n", dir, file_input_name);
+  char* dir_string = strdup(filename);
+  char* file_string = strdup(filename);
 
-  FILE *file_input;
-  // FILE *file_output;
-  char buffer[buffer_size];
+  char* dir = dirname(dir_string);
+  strcat(dir, "/");
+  char* file_output_name = basename(file_string);
 
-  file_input = fopen(filename, "r");
-  // file_output = fopen(output_file_name, "w");
+  file_output_name = swap_extension(file_output_name);
+  strcat(dir, file_output_name);
 
-  while (fgets(buffer, buffer_size, file_input)) {
-    printf("%s", buffer);
-    char buffer_output[buffer_size];
-    int count = 0;
-    int output = 0;
-    int first_index = 0;
-    char run_char = ' ';
-    int run = 0;
+  printf("%s\n", dir);
 
-    for (int i = 0; i < buffer_size; i++) {
-      buffer_output[buffer_size] = '\0';
-    }
-
-    printf("================= Next Line ===================\n");
-    printf("%s\n", buffer);
-
-    for (int i = 0; i < buffer_size; i++) {
-      if (buffer[i] == '\n') continue;
-      if (buffer[i] == '\0') break;
-      else if (buffer[i] == buffer[i-1] || i == 0) {
-        printf("Buffer i: %c\n ", buffer[i]);
-        if (run == 0) {
-          printf("Entering run\n");
-          run = 1;
-          first_index = i-1;
-          run_char = buffer[i];
-          printf("RUN CHAR: %c\n", run_char);
-        }
-        count++;
-        printf("Run length: %d\n", count);
-      } else if (buffer[i] != buffer[i-1]) {
-        if (run == 1) {
-          printf("Exiting run\n");
-          run = 0;
-          if (count >= 10) {
-            printf("AAAAA\n");
-            int num_digits = 0;
-            int c = count;
-            while (c != 0) {
-              c = c / 10;
-              num_digits++;
-              printf("Num digits: %d\n", num_digits);
-            }
-            char num[num_digits-1];
-            sprintf(num, "%d", count);
-            for (int j = 0; j < num_digits; j++) {
-              buffer_output[output++] = num[j];
-            }
-          } else {
-            buffer_output[output++] = count + '0';
-          }
-          buffer_output[output++] = run_char;
-
-          printf("Run char: %c\n", run_char);
-          printf("After run: %s\n", buffer_output);
-          count = 0;
-        } else {
-          printf("R: %c\n", buffer[i]);
-          run_char = buffer[i];
-          buffer_output[output++] = '1';
-          buffer_output[output++] = run_char;
-        }
-      }
-    }
-  }
-
-  fclose(file_input);
-  // fclose(file_output);
+  file_output = fopen(dir, "w");
+  fprintf(file_output, buffer_output);
+  fclose(file_output);
 
   return 0;
+}
+
+char* swap_extension(char* file) {
+  char* new_file;
+  new_file = strtok(file, ".");
+
+  strcat(new_file, output_file_type);
+
+  return new_file;
 }
 
 int unzipRLE(char* filename) {
